8696ae4595d871fd81040edbb5f3caceeb298a6c
Alexander Martinz <eviscerationls@gmail.com>
2013-05-24 14:28:58 +0200

    Updated and Cleaned Files

--- src/net/openfiresecurity/helper/Constants.java
+++ src/net/openfiresecurity/helper/Constants.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2013. Alexander Martinz.
+ */
+
 package net.openfiresecurity.helper;
 
 import android.annotation.SuppressLint;
--- src/net/openfiresecurity/imageuploader/MainActivity.java
+++ src/net/openfiresecurity/imageuploader/MainActivity.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2013. Alexander Martinz.
+ */
+
 package net.openfiresecurity.imageuploader;
 
 import android.annotation.SuppressLint;
